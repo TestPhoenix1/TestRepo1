4e4570a0a15608135adaad1bbfadd9d4232f2212
SierraTeachesStuff <sierra@learncoach.com>
2020-04-10 14:41:09 +1200

    change object  color on mouse over

---
+++ Assets/ColorChanger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    void OnMouseEnter()
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+}
